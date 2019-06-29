9d9ff78183dc59ff6f36a9acad676df727ba28b8
Koichi Nakamura <koichi@idein.jp>
2019-06-30 07:40:05 +0900

    Add a test

--- test/test_camera_projection.py
+++ test/test_camera_projection.py
@@ -6,7 +6,20 @@ import chainer_graphics.camera as C
 BATCH = 10
 POINTS = 100
 
-def test_projection():
+def test_cam2pixel():
+    x0 = np.random.randn(1, POINTS, 3).astype('float32')
+    focal = 1 + np.random.randn(1,2).astype('float32') / 100
+    offset = np.random.randn(1,2).astype('float32') / 100
+    K = C.camera_matrix(focal, offset)
+
+    rvec = np.zeros(3)
+    tvec = np.zeros(3)
+
+    p0, _ = cv2.projectPoints(x0[0], rvec, tvec, K.data[0], None)
+    p1 = C.cam2pixel(K, x0.transpose((0, 2, 1))).data.transpose((2, 0, 1))
+    assert(np.allclose(p0, p1))
+
+def test_reversibility():
     x0 = np.random.randn(BATCH, 3, POINTS).astype('float32')
     focal = 1 + np.random.randn(BATCH, 2).astype('float32') / 100
     offset = np.random.randn(BATCH, 2).astype('float32') / 100
