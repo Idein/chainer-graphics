d0fecd1546ed2c06845adeed17892530f23d641c
Koichi Nakamura <koichi@idein.jp>
2019-06-30 01:42:40 +0900

    Add warp_dense and warp_perspective

--- chainer_graphics/image/warp.py
+++ chainer_graphics/image/warp.py
@@ -1,10 +1,71 @@
 from chainer import backend
 import chainer.functions as F
 
+def affine(A, t, x):
+    """Compute Ax+t
+
+    Args:
+        A (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 3-D array of shape `(B, M, M)`
+        t (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 2-D array of shape `(B, M)`
+        x (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 2-D array of shape `(B, M, N)`
+
+    Returns:
+        ~chainer.Variable:
+            A 2-D array of shape `(B, M, N)`
+    """
+    xp = backend.get_array_module(A)
+    return A @ x + xp.expand_dims(t, axis=2)
+
+def inverse_affine(A, t, x):
+    """Compute A^-1(x-t)
+
+    Args:
+        A (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 3-D array of shape `(B, M, M)`
+        t (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 2-D array of shape `(B, M)`
+        x (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 2-D array of shape `(B, M, N)`
+
+    Returns:
+        ~chainer.Variable:
+            A 2-D array of shape `(B, M, N)`
+    """
+
+    xp = backend.get_array_module(A)
+    return F.batch_inv(A) @ (x - xp.expand_dims(t, axis=2))
+
 def pixel_coords(xp, H, W, dtype):
+    """Generate pixel coordinates"""
     us, vs = xp.meshgrid(xp.arange(W).astype(dtype), xp.arange(H).astype(dtype))
     ps = F.stack((us, vs), axis=0)
     return ps
+
+def warp_dense(image, ps):
+    """Dense image warping
+
+    Args:
+        image (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 4-D array of shape `(B, C, H, W)`.
+
+        ps (:class `~chainer.Variable` or :ref:`ndarray`):
+            A 4-D array of shape `(B, 2, H, W)`
+            Pixel coordinates in source images.
+
+    Returns:
+        ~chainer.Variable:
+            Warped image.
+            A 4-D array of shape `(B, C, H, W)`.
+    """
+    xp = backend.get_array_module(image)
+    B, _, H, W = image.shape
+    ps = 2 * ps / xp.array([W-1, H-1]).reshape(-1, 2, 1) - 1
+    ps = ps.reshape(B, 2, H, W)
+
+    return F.spatial_transformer_sampler(image, ps)
 
 def warp_affine(image, mat):
     """Warp images with affine transformation
@@ -25,17 +86,9 @@ def warp_affine(image, mat):
     xp = backend.get_array_module(image)
     B, _, H, W = image.shape
 
-    A = mat[:, :, :2]
-    Ainv = F.batch_inv(A)
-    t = mat[:, :, 2].reshape(-1, 2, 1)
     ps1 = pixel_coords(xp, H, W, mat.dtype).reshape(1, 2, -1)
-
-    ps0 = Ainv @ (ps1 - t)
-    ps0 = 2 * ps0 / xp.array([W-1, H-1]).reshape(-1, 2, 1) - 1
-    ps0 = ps0.reshape(B, 2, H, W)
-
-    warped_image = F.spatial_transformer_sampler(image, ps0)
-    return warped_image
+    ps0 = inverse_affine(mat[:, :, :2], mat[:, :, 2], ps1)
+    return warp_dense(image, ps0)
 
 def warp_perspective(image, mat):
     """Warp images with perspective transformation
@@ -45,8 +98,8 @@ def warp_perspective(image, mat):
             A 4-D array of shape `(B, C, H, W)`.
 
         mat (:class `~chainer.Variable` or :ref:`ndarray`):
-            Affine transformation matrices [[a, b, tx], [c, d, ty]].
-            A 3-D array of shape `(B, 2, 3)`.
+            Perspective transformaion matrices.
+            A 3-D array of shape `(B, 3, 3)`.
 
     Returns:
         ~chainer.Variable:
@@ -56,10 +109,8 @@ def warp_perspective(image, mat):
     xp = backend.get_array_module(image)
     B, _, H, W = image.shape
 
-    pixel_coords(xp, H, W, mat.dtype) 
-
-def warp_dense(image):
-    #x -> x'
-    #cam2pixel(x) -> cam2pixel(x')
-    #pass
-    pass
+    ps1 = pixel_coords(xp, H, W, mat.dtype).reshape(1, 2, -1)
+    num   = affine(mat[:,:2,:2], mat[:,:2,2], ps1)
+    denom = affine(mat[:,2,:2].reshape(-1, 1, 2), mat[:,2,2].reshape(-1, 1), ps1)
+    ps0 = num / denom
+    return warp_dense(image, ps0)
--- test/test_warp.py
+++ test/test_warp.py
@@ -3,7 +3,7 @@ import cv2
 
 import chainer_graphics.image as I
 
-eps = 1e-5
+eps = 1e-3
 
 def cosine_similarity(image0, image1):
     v0 = image0.flatten()
@@ -21,14 +21,36 @@ def test_warp_affine():
     identity = np.array([[1., 0., 0.], [0., 1., 0.]])
     mat = identity + np.random.randn(2, 3)/10
 
-    image = cv2.imread("test/lena.png").astype('f')
+    image = cv2.imread("test/lena.png").astype(float)
     H, W, _ = image.shape
 
     ref_image = cv2.warpAffine(image, mat, (W, H))
 
     image = np.expand_dims(image.transpose((2, 0, 1)), axis=0)
     mat = np.expand_dims(mat, axis=0)
-    warped_image = I.warp_affine(image.astype(float), mat).data
+    warped_image = I.warp_affine(image, mat).data
     warped_image = warped_image.transpose((0, 2, 3, 1)).reshape((H, W, 3))
+
+    #cv2.imwrite('ref.png', ref_image)
+    #cv2.imwrite('warped.png', warped_image)
+
+    assert(1 - cosine_similarity(ref_image, warped_image) < eps)
+
+def test_warp_perspective():
+    identity = np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+    mat = identity + np.random.randn(3, 3) / 1000
+
+    image = cv2.imread("test/lena.png").astype(float)
+    H, W, _ = image.shape
+
+    ref_image = cv2.warpPerspective(image, mat, (W, H), flags=cv2.WARP_INVERSE_MAP)
+
+    image = np.expand_dims(image.transpose((2, 0, 1)), axis=0)
+    mat = np.expand_dims(mat, axis=0)
+    warped_image = I.warp_perspective(image, mat).data
+    warped_image = warped_image.transpose((0, 2, 3, 1)).reshape((H, W, 3))
+
+    #cv2.imwrite('ref.png', ref_image)
+    #cv2.imwrite('warped.png', warped_image)
 
     assert(1 - cosine_similarity(ref_image, warped_image) < eps)
